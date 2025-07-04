4bd58fd6447ee020e0b985181c3cca5a908890ab
kritikaverrma <kritikaverma51510@gmail.com>
2025-07-04 18:37:10 +0530

    Updates

--- _next/static/zSdvVxI2Yb0g6zhsyYWd5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/863-91733211f1574310.js","static/chunks/pages/index-e7784682c544c928.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
